23e5b4d4d2b2a2051aec217f0ed6804d7f1e8096
Maria G <ruiined@gmail.com>
2024-12-01 23:25:06 +0000

    feat: day 1 solution

--- day1/solve.ts
+++ day1/solve.ts
@@ -1,17 +1,31 @@
 import { solve } from '../utils';
 
-type Input = string[][];
+type Input = { left: number[]; right: number[] };
+
+const sort = (input: number[]) => input.sort((a, b) => a - b);
+
+const mapByIdx = (input: string[][], idx: number) =>
+  input.map((line) => Number(line[idx]));
 
 function parser(input: string): Input {
-  return input.split('\n').map((l) => l.split(''));
+  const arr = input
+    .split('\n')
+    .map((l) => l.split(' ').filter((l) => l !== ''));
+
+  return { left: sort(mapByIdx(arr, 0)), right: sort(mapByIdx(arr, 1)) };
 }
 
-function part1(input: Input) {
-  return input;
+function part1({ left, right }: Input) {
+  return left.reduce((acc, num, i) => acc + Math.abs(num - right[i]), 0);
 }
 
-function part2(input: Input) {
-  return input;
+function part2({ left, right }: Input) {
+  const count = right.reduce((acc, num) => {
+    acc[num] = (acc[num] ?? 0) + 1;
+    return acc;
+  }, {});
+
+  return left.reduce((acc, num) => acc + num * (count[num] ?? 0), 0);
 }
 
-solve({ part1, test1: [], part2, test2: [], parser });
+solve({ part1, test1: 11, part2, test2: 31, parser });
